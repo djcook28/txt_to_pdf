0c1353a11d8b095471075d2969a04ac554a0ff68
dcook <daniel.j.cook.28@gmail.com>
2025-03-23 17:49:56 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import glob
+from fpdf import FPDF
+
+filepaths = glob.glob("Text Files/*.txt")
+
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+
+for filepath in filepaths:
+    filename = Path(filepath).stem
+
+
+    pdf.set_font(family="Times", size=16, style='B')
+    pdf.set_text_color(0, 0, 0)
+
+    pdf.add_page()
+    pdf.cell(w=50, h=16, txt=f'{filename.title()}', ln=1)
+
+pdf.output("PDFs/output.pdf")
